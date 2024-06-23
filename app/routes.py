8be4b162a4495aa5c638f8a6357f3b8c90b72cb4
Polly <polly@Pollys-MacBook-Air.local>
2024-06-23 15:12:01 +0300

    added tests for solver, added solve route

--- app/routes.py
+++ app/routes.py
@@ -1,76 +1,43 @@
-from typing import Any, Union, Tuple
+from typing import Any, Union, Tuple, Annotated
 
 from flask import Blueprint, request, jsonify, Response
-from .sudoku import Sudoku
+from .sudoku_gen import SudokuGen
+
+# from sudoku import Sudoku
 
 main = Blueprint('main', __name__)
 
 
-@main.route('/create_standard', methods=['GET'])
-def create_standard() -> Response:
+@main.route('/generate/', methods=['GET'])
+def generate() -> Response:
     """
-    creates easy level sudoku puzzle
+    creates a sudoku puzzle with levels from 1 to 3
+    and any width (min 2)
+
     :return:JSON response
-    """
-    sudoku = Sudoku.create_sudoku()
-    return jsonify({"sudoku": sudoku})
+    # """
+    diff = request.args.get('diff') if request.args.get('level') is not None else 0.6
+    width = request.args.get('width') if request.args.get('width') is not None else 3
+    # puzzle = Sudoku(int(width)).difficulty(float(diff))
+    puzzle = SudokuGen.generate(width=int(width), difficulty=int(diff))
+    return jsonify(puzzle.__dict__["board"])
 
 
-@main.route('/create_level', methods=['POST'])
-def create_level() -> Response | Tuple[Response, int]:
-    """
-    creates chosen by user level sudoku puzzle
-    :return: JSON response
-    """
-    data = request.get_json()
-    diff = data.get('difficulty')
-    if diff not in [1, 2, 3]:
-        return jsonify({"error": "Invalid difficulty level"}), 400
-    sudoku = Sudoku.create_sudoku(diff=diff)
-    return jsonify({"sudoku": sudoku})
-
-
-@main.route('/create_custom', methods=['POST'])
-def create_custom() -> Response | Tuple[Response, int]:
-    """
-    validates custom sudoku
-    :return: JSON response
-    """
-    data = request.get_json()
-    nums = data.get('nums')
-    valid = Sudoku.check_valid(nums=nums)
-    if not valid:
-        return jsonify({"error": "Invalid custom numbers"}), 400
-    sudoku = Sudoku.create_custom(nums=nums)
-    return jsonify({"sudoku": sudoku})
-
-
-@main.route('/solve', methods=['POST'])
+@main.route('/solve', methods=['GET'])
 def solve() -> Response | Tuple[Response, int]:
     """
     solves sudoku that is sent by user
     :return: JSON response
     """
-    data = request.get_json()
-    nums = data.get('nums')
-    valid = Sudoku.check_valid(nums=nums)
-    if not valid:
-        return jsonify({"error": "Invalid sudoku"}), 400
-    solved = Sudoku.solve(nums=nums)
-    return jsonify({"solved": solved})
+    to_solve = request.get_json()['arr']
+    result = SudokuGen.solve(to_solve)
+    return jsonify(result)
 
 
-@main.route('/verify', methods=['POST'])
+@main.route('/verify', methods=['GET'])
 def verify() -> Response | Tuple[Response, int]:
     """
     verifies sudoku that is sent by user
     :return: JSON response
     """
-    data = request.get_json()
-    nums = data.get('nums')
-    valid = Sudoku.check_valid(nums=nums)
-    if not valid:
-        return jsonify({"error": "Invalid sudoku"}), 400
-    result = Sudoku.verify(nums=nums)
-    return jsonify({"result": result})
-
+    pass
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,42 +7,57 @@ class TestRoutes(unittest.TestCase):
     #     app = create_app()
     #     client = app.test_client()
 
-    def test_create_standard(self):
+    def test_generate(self):
         app = create_app()
         client = app.test_client()
-        response = client.get('/create_standard')
+        diff = 0.6
+        width = 2
+        response = client.get(f'/generate/?diff={diff}&width={width}')
+        response_no_headers = client.get(f'/generate/')
         self.assertEqual(response.status_code, 200)
-
-    def test_create_level(self):
-        app = create_app()
-        client = app.test_client()
-        for difficulty in [1, 2, 3]:
-            response = client.post('/create_level', json={'difficulty': difficulty})
-            self.assertEqual(response.status_code, 200)
-
-    def test_create_custom(self):
-        app = create_app()
-        client = app.test_client()
-
-        custom_sudoku = [1, 2, 3]
-        response = client.post('/create_custom', json={'nums': custom_sudoku})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_no_headers.status_code, 200)
 
     def test_solve(self):
+        arr = [
+            [3, 0, 6, 5, 0, 8, 4, 0, 0],
+            [5, 2, 0, 0, 0, 0, 0, 0, 0],
+            [0, 8, 7, 0, 0, 0, 0, 3, 1],
+            [0, 0, 3, 0, 1, 0, 0, 8, 0],
+            [9, 0, 0, 8, 6, 3, 0, 0, 5],
+            [0, 5, 0, 0, 9, 0, 6, 0, 0],
+            [1, 3, 0, 0, 0, 0, 2, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 7, 4],
+            [0, 0, 5, 2, 0, 6, 3, 0, 0]
+        ]
         app = create_app()
         client = app.test_client()
+        response = client.get(f'/solve', json={'arr': arr})
+        self.assertEqual(200, response.status_code)
 
-        user_sudoku = [1, 2, 3]
 
-        response = client.post('/solve', json={'nums': user_sudoku})
-        self.assertEqual(response.status_code, 200)
-
-    def test_verify(self):
-        app = create_app()
-        client = app.test_client()
-        user_sudoku = [1, 2, 3]
-        response = client.post('/verify', json={'nums': user_sudoku})
-        self.assertEqual(response.status_code, 200)
+    # def test_create_custom(self):
+    #     app = create_app()
+    #     client = app.test_client()
+    #
+    #     custom_sudoku = [1, 2, 3]
+    #     response = client.post('/create_custom', json={'nums': custom_sudoku})
+    #     self.assertEqual(response.status_code, 200)
+    #
+    # def test_solve(self):
+    #     app = create_app()
+    #     client = app.test_client()
+    #
+    #     user_sudoku = [1, 2, 3]
+    #
+    #     response = client.post('/solve', json={'nums': user_sudoku})
+    #     self.assertEqual(response.status_code, 200)
+    #
+    # def test_verify(self):
+    #     app = create_app()
+    #     client = app.test_client()
+    #     user_sudoku = [1, 2, 3]
+    #     response = client.post('/verify', json={'nums': user_sudoku})
+    #     self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
