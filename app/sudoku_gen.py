7cc17ecca5122d05fd8d93219674cd7a9cde50cf
Polly <polly@Pollys-MacBook-Air.local>
2024-06-28 16:52:53 +0300

    added board class

--- app/sudoku_gen.py
+++ app/sudoku_gen.py
@@ -1,5 +1,6 @@
+from math import sqrt
+
 from sudoku import Sudoku
-import copy
 
 
 class SudokuGen:
@@ -16,20 +17,59 @@ class SudokuGen:
         return puzzle.board
 
     @staticmethod
-    def solve(arr: list[list[int]]) -> list[list[int]] or None:
-        if arr is None:
+    def solve(arr: list[int], width: int = 3, height: int = 3) -> list[list[int]] or None:
+        board = Board.transform_into_board(arr=arr)
+        if board is None:
             return None
-        puzzle = Sudoku(3, 3, board=arr)
-        if puzzle.validate() is False:
+        board = Board.transform_into_board(arr=arr)
+        puzzle = Sudoku(width=width, height=height, board=board)
+        if puzzle.validate() is False or not board:
             return None
         return puzzle.solve().board
 
     @staticmethod
-    def validate(arr: list[list[int]]) -> bool:
-        if arr is None:
+    def validate(arr: list[int], width: int = 3, height: int = 3) -> bool:
+        board = Board.transform_into_board(arr=arr)
+        if board is None:
+            return False
+        board = Board.transform_into_board(arr=arr)
+        if not board:
             return False
         try:
-            puzzle = Sudoku(3, 3, board=arr)
+            puzzle = Sudoku(width=width, height=height, board=board)
         except (IndexError, TypeError, ValueError):
             return False
         return puzzle.validate()
+
+    @staticmethod
+    def generate_custom(arr: list[int], width: int = 3, height: int = 3) -> list[list[int]] or None:
+        board = Board.transform_into_board(arr=arr)
+
+
+class Board:
+    valid_lengths = (16, 81, 256)
+
+    @staticmethod
+    def transform_into_board(arr: list[int]) -> list[list[int]]:
+        """
+        Transforms array into 2D array
+        :return: List[List[int]]
+        """
+        if Board.__validate(arr):
+            board = []
+            board_len = len(arr)
+            num_cells = int(sqrt(board_len))
+            for _cell in range(0, board_len, num_cells):
+                cell = []
+                for square in range(num_cells):
+                    cell.append(arr[square + _cell])
+                board.append(cell)
+            return board
+
+    @classmethod
+    def __validate(cls, arr) -> bool:
+        if arr is None:
+            return False
+        if len(arr) not in cls.valid_lengths:
+            return False
+        return True
--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,61 +1,50 @@
 import random
 import unittest
 
-from app.sudoku_gen import SudokuGen
+from app.sudoku_gen import SudokuGen, Board
 
 
-class TestSolver(unittest.TestCase):
+class TestGenerator(unittest.TestCase):
+    valid_unsolved = [0, 0, 7, 0, 4, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 8, 0, 0, 6,
+                      0, 4, 1, 0, 0, 0, 9, 0, 0,
+                      0, 0, 0, 0, 0, 0, 1, 7, 0,
+                      0, 0, 0, 0, 0, 6, 0, 0, 0,
+                      0, 0, 8, 7, 0, 0, 2, 0, 0,
+                      3, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 1, 2, 0, 0, 0, 0,
+                      8, 6, 0, 0, 7, 0, 0, 0, 5]
+
+    solved = [[9, 8, 7, 6, 4, 2, 5, 3, 1],
+              [2, 3, 5, 9, 1, 8, 7, 4, 6],
+              [6, 4, 1, 5, 3, 7, 9, 8, 2],
+              [5, 2, 6, 3, 8, 4, 1, 7, 9],
+              [1, 7, 3, 2, 9, 6, 8, 5, 4],
+              [4, 9, 8, 7, 5, 1, 2, 6, 3],
+              [3, 1, 9, 8, 6, 5, 4, 2, 7],
+              [7, 5, 4, 1, 2, 3, 6, 9, 8],
+              [8, 6, 2, 4, 7, 9, 3, 1, 5]]
 
     def test_generate(self):
         puzzle = SudokuGen.generate(width=3, difficulty=0.6)
         self.assertEqual(len(puzzle), 9)
 
     def test_solve(self):
-        board = [
-            [0, 0, 7, 0, 4, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 8, 0, 0, 6],
-            [0, 4, 1, 0, 0, 0, 9, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 7, 0],
-            [0, 0, 0, 0, 0, 6, 0, 0, 0],
-            [0, 0, 8, 7, 0, 0, 2, 0, 0],
-            [3, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 2, 0, 0, 0, 0],
-            [8, 6, 0, 0, 7, 0, 0, 0, 5]
-        ]
-
-        solved = [[9, 8, 7, 6, 4, 2, 5, 3, 1],
-                  [2, 3, 5, 9, 1, 8, 7, 4, 6],
-                  [6, 4, 1, 5, 3, 7, 9, 8, 2],
-                  [5, 2, 6, 3, 8, 4, 1, 7, 9],
-                  [1, 7, 3, 2, 9, 6, 8, 5, 4],
-                  [4, 9, 8, 7, 5, 1, 2, 6, 3],
-                  [3, 1, 9, 8, 6, 5, 4, 2, 7],
-                  [7, 5, 4, 1, 2, 3, 6, 9, 8],
-                  [8, 6, 2, 4, 7, 9, 3, 1, 5]]
+        board = self.valid_unsolved
 
         result = SudokuGen.solve(board)
-        self.assertEqual(result, solved)
+        self.assertEqual(result, self.solved)
 
     def test_solve_nan(self):
         board = None
-        board2 = [[random.randint(1, 9) for _ in range(10)]]
+        board2 = [random.randint(1, 9) for _ in range(10)]
         result = SudokuGen.solve(board)
         result2 = SudokuGen.solve(board2)
         self.assertEqual(result, None)
         self.assertEqual(result2, None)
 
     def test_validate(self):
-        board = [
-            [0, 0, 7, 0, 4, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 8, 0, 0, 6],
-            [0, 4, 1, 0, 0, 0, 9, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 7, 0],
-            [0, 0, 0, 0, 0, 6, 0, 0, 0],
-            [0, 0, 8, 7, 0, 0, 2, 0, 0],
-            [3, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 2, 0, 0, 0, 0],
-            [8, 6, 0, 0, 7, 0, 0, 0, 5]
-        ]
+        board = self.valid_unsolved
         board2 = None
         board3 = [1, 2, 3]
         result = SudokuGen.validate(board)
@@ -64,3 +53,16 @@ class TestSolver(unittest.TestCase):
         self.assertEqual(result, True)
         self.assertEqual(result2, False)
         self.assertEqual(result3, False)
+
+    def test_generate_custom_board(self):
+        board = self.valid_unsolved
+
+    def test_board_to_2d(self):
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7]
+        arr2 = [1, 4, 5]
+        result = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 1, 2, 3], [4, 5, 6, 7]]
+        result2 = None
+        board = Board.transform_into_board(arr)
+        board2 = Board.transform_into_board(arr2)
+        self.assertEqual(result, board)
+        self.assertEqual(result2, board2)
